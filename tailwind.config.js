ab31254078db76e3c99f488c7552679222c14951
Bilime <bilime1030@gmail.com>
2025-01-08 11:36:24 +0900

    Chore: add types folder

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{ts,tsx}"],
   theme: {
+    screens: {
+      tablet: "640px",
+      laptop: "1024px",
+      desktop: "1280px",
+    },
     extend: {},
   },
   plugins: [],
