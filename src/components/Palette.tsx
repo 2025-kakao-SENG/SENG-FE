9c88c268a7d51cb161c016c1c8a2fc1ab90c0a99
Green-JEONG <th2gr22n@gmail.com>
2025-01-09 19:37:19 +0900

    Feat: created palette bar component

---
+++ src/components/Palette.tsx
@@ -0,0 +1,51 @@
+import React, { useEffect } from 'react';
+import hightlight from '../assets/images/palette/hightlight.svg';
+import downArrow from '../assets/images/palette/downArrow.svg';
+import grid from '../assets/images/palette/grid.svg';
+import checkBox from '../assets/images/palette/checkBox.svg';
+import pen from '../assets/images/palette/pen.svg';
+import text from '../assets/images/palette/text.svg';
+import chat from '../assets/images/palette/chat.svg';
+import interests from '../assets/images/palette/interests.svg';
+import aiYellow from '../assets/images/aiYellow.svg';
+
+export default function Palette() {
+
+    return (
+        <>
+            <div className="flex bg-[#111111] border border-[#DBAC4A] rounded-[0.575rem]">
+
+                <div className="flex items-center pl-[0.2875rem] py-[0.2875rem] pr-[0.43125rem] gap-[0.43125rem]">
+                    <div className="flex">
+                        <img src={hightlight} alt="" className="p-[0.43125rem]" />
+                        <img src={downArrow} alt="" className="" />
+                    </div>
+
+                    <div className="flex">
+                        <img src={grid} alt="" className="p-[0.43125rem]" />
+                        <img src={downArrow} alt="" className="" />
+                    </div>
+
+                    <div className="flex">
+                        <img src={checkBox} alt="" className="p-[0.43125rem]" />
+                        <img src={downArrow} alt="" className="" />
+                    </div>
+
+                    <div className="flex">
+                        <img src={pen} alt="" className="p-[0.43125rem]" />
+                        <img src={downArrow} alt="" className="" />
+                    </div>
+                    
+                    <img src={text} alt="" className="p-[0.43125rem]" />
+                    <img src={chat} alt="" className="p-[0.43125rem]" />
+                    <img src={interests} alt="" className="p-[0.43125rem]" />
+                </div>
+
+                <div className="flex justify-center items-center pt-1.5 pb-[0.6875rem] pl-[1.1875rem] pr-6 border-l border-[#444444]">
+                    <img src={aiYellow} alt="" className=" " />
+                </div>
+
+            </div>
+        </>
+    )
+}
