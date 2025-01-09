e69067718d851186cffeda5f014897c702f6a85c
Green-JEONG <th2gr22n@gmail.com>
2025-01-10 00:46:08 +0900

    Feat: created Side Bar Component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import Palette from '../components/palette';
 
 const Home: React.FC = () => {
     return (
-        <div className="flex min-h-screen bg-black">
-            <h1 className="text-white">This is the home page.</h1>
-        </div>
+        <>
+            <h1 className="">This is the home page.</h1>
+        </>
     );
 };
 
