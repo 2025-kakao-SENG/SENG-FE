7f15c305bdf845d44b87fbae60d8e8421a89b3c5
Green-JEONG <th2gr22n@gmail.com>
2025-01-09 19:37:19 +0900

    Feat: created palette bar component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Palette from '../components/palette';
 
 const Home: React.FC = () => {
     return (
-        <div className="flex flex-col items-center justify-center min-h-screen bg-black">
-            <h1 className="">This is the home page.</h1>
+        <div className="flex min-h-screen bg-black">
+            <h1 className="text-white">This is the home page.</h1>
         </div>
     );
 };
