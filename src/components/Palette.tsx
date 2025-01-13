30257162bc60a62d9f8295988b4857d1f7c90368
Green-JEONG <th2gr22n@gmail.com>
2025-01-13 14:09:52 +0900

    Feat: created Login Modal

--- src/components/Palette.tsx
+++ src/components/Palette.tsx
@@ -17,32 +17,37 @@ export default function Palette() {
 
                 <div className="flex items-center pl-[0.2875rem] py-[0.2875rem] pr-[0.43125rem] gap-[0.43125rem]">
                     <div className="flex">
+<<<<<<< Updated upstream
                         <img src={highlight} alt="" className="p-[0.43125rem]" />
                         <img src={downArrow} alt="" className="" />
+=======
+                        <img src={highlight} alt="Highlight Button" className="p-[0.43125rem]" />
+                        <img src={downArrow} alt="Down-arrow Button" className="" />
+>>>>>>> Stashed changes
                     </div>
 
                     <div className="flex">
-                        <img src={grid} alt="" className="p-[0.43125rem]" />
-                        <img src={downArrow} alt="" className="" />
+                        <img src={grid} alt="Grid Button" className="p-[0.43125rem]" />
+                        <img src={downArrow} alt="Down-arrow Button" className="" />
                     </div>
 
                     <div className="flex">
-                        <img src={checkBox} alt="" className="p-[0.43125rem]" />
-                        <img src={downArrow} alt="" className="" />
+                        <img src={checkBox} alt="CheckBox Button" className="p-[0.43125rem]" />
+                        <img src={downArrow} alt="Down-arrow Button" className="" />
                     </div>
 
                     <div className="flex">
-                        <img src={pen} alt="" className="p-[0.43125rem]" />
-                        <img src={downArrow} alt="" className="" />
+                        <img src={pen} alt="Pen Button" className="p-[0.43125rem]" />
+                        <img src={downArrow} alt="Down-arrow Button" className="" />
                     </div>
                     
-                    <img src={text} alt="" className="p-[0.43125rem]" />
-                    <img src={chat} alt="" className="p-[0.43125rem]" />
-                    <img src={interests} alt="" className="p-[0.43125rem]" />
+                    <img src={text} alt="Text Button" className="p-[0.43125rem]" />
+                    <img src={chat} alt="Chat Button" className="p-[0.43125rem]" />
+                    <img src={interests} alt="Interests Button" className="p-[0.43125rem]" />
                 </div>
 
                 <div className="flex justify-center items-center pt-1.5 pb-[0.6875rem] pl-[1.1875rem] pr-6 border-l border-[#444444]">
-                    <img src={aiYellow} alt="" className=" " />
+                    <img src={aiYellow} alt="Yellow Ai Button" className="" />
                 </div>
 
             </div>
