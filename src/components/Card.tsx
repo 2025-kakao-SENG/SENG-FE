0989676e445c63438abc649c85849b7093c39187
Green-JEONG <th2gr22n@gmail.com>
2025-01-16 11:47:42 +0900

    Feat: created library page and modified layout included setting of scroll

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,45 +1,29 @@
 import React from 'react';
-import bookTest from '../assets/images/bookTest.svg';
 
-const Card: React.FC = () => {
-  // 내부에서 사용할 데이터
-  const books = [
-    {
-      id: 1,
-      title: '오늘하루도, 소심한 야옹이',
-      cover: bookTest, // 실제 이미지 경로로 교체
-      link: '/read/cat/',
-    },
-    {
-      id: 2,
-      title: '오늘하루도, 소심한 야옹이',
-      cover: bookTest, // 실제 이미지 경로로 교체
-      link: '/read/cat/',
-    },
-  ];
+const Card: React.FC<{
+    books: Array<{id: number; title: string; cover: string; link: string}>;
+}> = ({books}) => {
+    const handleRead = (link: string) => {
+        // eBook 읽기 페이지로 이동
+        window.location.href = link;
+    };
 
-  const handleRead = (link: string) => {
-    // eBook 읽기 페이지로 이동
-    window.location.href = link;
-  };
-
-  return (
-    <div className="bookshelf flex flex-wrap justify-center gap-9">
-      {books.map((book) => (
-        <div
-          key={book.id}
-          className="w-[10.5rem] h-[16.5rem] cursor-pointer transform transition-transform duration-200 hover:drop-shadow-glow-yellow hover:animate-wiggle rounded-2xl overflow-hidden"
-          onClick={() => handleRead(book.link)}
-        >
-          <img
-            src={book.cover}
-            alt={`${book.title} cover`}
-            className="w-full h-full object-cover transition-opacity duration-300 opacity-80 hover:opacity-100"
-          />
+    return (
+        <div className="bookshelf flex flex-wrap gap-[2.3125rem]">
+            {books.map(book => (
+                <div
+                    key={book.id}
+                    className="h-[16.5rem] w-[10.5rem] transform cursor-pointer overflow-hidden rounded-2xl transition-transform duration-200 hover:animate-wiggle hover:drop-shadow-glow-yellow"
+                    onClick={() => handleRead(book.link)}>
+                    <img
+                        src={book.cover}
+                        alt={`${book.title} cover`}
+                        className="h-full w-full object-cover opacity-80 transition-opacity duration-300 hover:opacity-100"
+                    />
+                </div>
+            ))}
         </div>
-      ))}
-    </div>
-  );
+    );
 };
 
 export default Card;
--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -3,13 +3,13 @@ import MenuBar from '../components/MenuBar';
 
 function Layout() {
     return (
-        <div className="flex h-screen bg-black text-white">
-            <header className="pb-[1.75rem] pl-[1rem]">
+        <div className="flex min-h-screen bg-black text-white">
+            <header className="h-screen pb-[1.75rem] pl-[1rem]">
                 <nav>
                     <MenuBar />
                 </nav>
             </header>
-            <main>
+            <main className="h-screen overflow-hidden">
                 <Outlet />
             </main>
         </div>
