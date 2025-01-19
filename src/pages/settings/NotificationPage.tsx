05431ea3e784c8dc8915f96127665ac9e9b49666
Green-JEONG <th2gr22n@gmail.com>
2025-01-20 04:19:16 +0900

    Fix: modified layout and created tabs contents

--- src/pages/settings/NotificationPage.tsx
+++ src/pages/settings/NotificationPage.tsx
@@ -1,7 +1,122 @@
 function NotificationPage() {
     return (
-        <div>
-            <h1>Notification Settings</h1>
+        <div className="h-[40.05rem] overflow-y-scroll pr-[20.625rem]">
+            <h2 className="text-sm font-semibold text-[#F5F5F5]">
+                시스템 알림
+            </h2>
+            <div className="mt-[0.5625rem] flex flex-col flex-wrap gap-[0.5625rem]">
+                <div className="flex flex-col rounded-[0.1875rem] bg-[#292929] p-[0.9375rem]">
+                    <p className="text-xs font-bold text-[#999999]">
+                        500 토큰이 충전되었습니다.
+                    </p>
+                    <div className="my-1">
+                        <p className="text-xs text-[#999999]">
+                            안녕하세요 스토리브리즈입니다. Noah님의 토큰 충전
+                            내역을 안내드립니다.
+                        </p>
+                        <p className="text-xs text-[#999999]">
+                            2025년 01월 08일 13시 08분 등록하신 결제 수단으로
+                            500토큰이 충전되었습니다.
+                        </p>
+                    </div>
+                    <p className="self-end text-[0.625rem] text-[#999999]">
+                        2025-01-08
+                    </p>
+                </div>
+                <div className="flex flex-col rounded-[0.1875rem] bg-[#292929] p-[0.9375rem]">
+                    <p className="text-xs font-bold text-[#DBAC4A]">
+                        500 토큰이 충전되었습니다.
+                    </p>
+                    <div className="my-1">
+                        <p className="text-xs text-[#DBAC4A]">
+                            안녕하세요 스토리브리즈입니다. Noah님의 토큰 충전
+                            내역을 안내드립니다.
+                        </p>
+                        <p className="text-xs text-[#DBAC4A]">
+                            2025년 01월 08일 13시 08분 등록하신 결제 수단으로
+                            500토큰이 충전되었습니다.
+                        </p>
+                    </div>
+                    <p className="self-end text-[0.625rem] text-[#999999]">
+                        2025-01-08
+                    </p>
+                </div>
+                <div className="flex flex-col rounded-[0.1875rem] bg-[#292929] p-[0.9375rem]">
+                    <p className="text-xs font-bold text-[#999999]">
+                        500 토큰이 충전되었습니다.
+                    </p>
+                    <div className="my-1">
+                        <p className="text-xs text-[#999999]">
+                            안녕하세요 스토리브리즈입니다. Noah님의 토큰 충전
+                            내역을 안내드립니다.
+                        </p>
+                        <p className="text-xs text-[#999999]">
+                            2025년 01월 08일 13시 08분 등록하신 결제 수단으로
+                            500토큰이 충전되었습니다.
+                        </p>
+                    </div>
+                    <p className="self-end text-[0.625rem] text-[#999999]">
+                        2025-01-08
+                    </p>
+                </div>
+            </div>
+
+            <h2 className="mt-[2.1875rem] flex flex-col flex-wrap text-sm font-semibold text-[#F5F5F5]">
+                커뮤니티 알림
+            </h2>
+            <div className="mt-[0.5625rem] flex flex-col gap-[0.5625rem]">
+                <div className="flex flex-col rounded-[0.1875rem] bg-[#292929] p-[0.9375rem]">
+                    <p className="text-xs font-bold text-[#DBAC4A]">
+                        Eden님이 게시글에 댓글을 남겼습니다.
+                    </p>
+                    <div className="my-1">
+                        <p className="text-xs text-[#DBAC4A]">
+                            회원님의 “공룡책” 게시글에 Eden님이 댓글을
+                            남겼습니다.
+                        </p>
+                        <p className="text-xs text-[#DBAC4A]">
+                            Eden님의 댓글을 확인하고 소통해보세요.
+                        </p>
+                    </div>
+                    <p className="self-end text-[0.625rem] text-[#999999]">
+                        2025-01-08
+                    </p>
+                </div>
+                <div className="flex flex-col rounded-[0.1875rem] bg-[#292929] p-[0.9375rem]">
+                    <p className="text-xs font-bold text-[#999999]">
+                        Eden님이 게시글에 댓글을 남겼습니다.
+                    </p>
+                    <div className="my-1">
+                        <p className="text-xs text-[#999999]">
+                            회원님의 “공룡책” 게시글에 Eden님이 댓글을
+                            남겼습니다.
+                        </p>
+                        <p className="text-xs text-[#999999]">
+                            Eden님의 댓글을 확인하고 소통해보세요.
+                        </p>
+                    </div>
+                    <p className="self-end text-[0.625rem] text-[#999999]">
+                        2025-01-08
+                    </p>
+                </div>
+                <div className="flex flex-col rounded-[0.1875rem] bg-[#292929] p-[0.9375rem]">
+                    <p className="text-xs font-bold text-[#999999]">
+                        Eden님이 게시글에 댓글을 남겼습니다.
+                    </p>
+                    <div className="my-1">
+                        <p className="text-xs text-[#999999]">
+                            회원님의 “공룡책” 게시글에 Eden님이 댓글을
+                            남겼습니다.
+                        </p>
+                        <p className="text-xs text-[#999999]">
+                            Eden님의 댓글을 확인하고 소통해보세요.
+                        </p>
+                    </div>
+                    <p className="self-end text-[0.625rem] text-[#999999]">
+                        2025-01-08
+                    </p>
+                </div>
+            </div>
         </div>
     );
 }
