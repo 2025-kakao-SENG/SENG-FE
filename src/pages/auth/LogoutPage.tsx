3d8053a5a0154c4887aca74be669e0561be209c0
Green-JEONG <th2gr22n@gmail.com>
2025-02-02 15:18:35 +0900

    Fix: modified light mode

--- src/pages/auth/LogoutPage.tsx
+++ src/pages/auth/LogoutPage.tsx
@@ -27,15 +27,18 @@ function Logout({backgroundLocation: logoutBackgroundLocation}: LogoutProps) {
 
     return (
         <div
-            className={`absolute left-1/2 top-1/2 flex h-[9.75rem] w-[27.75rem] -translate-x-1/2 -translate-y-1/2 transform flex-col items-center rounded-2xl bg-[#1B1B1B] ${
+            className={`absolute left-1/2 top-1/2 flex h-[9.75rem] w-[27.75rem] -translate-x-1/2 -translate-y-1/2 transform flex-col items-center rounded-2xl ${
                 isDarkMode
                     ? 'bg-[#1B1B1B] text-white'
-                    : 'bg-[#f5f5f5] text-black'
+                    : 'bg-[#ffffff] text-black'
             }`}>
             {userLoginDate.isLogined ? (
                 <>
                     {/* 로그아웃 확인 메시지 */}
-                    <p className="mb-[2rem] mt-[2.2rem] text-[0.98rem] font-medium text-white">
+                    <p
+                        className={`mb-[2rem] mt-[2.2rem] text-[0.98rem] font-medium ${
+                            isDarkMode ? 'text-white' : 'text-black'
+                        }`}>
                         로그아웃을 진행하시겠습니까?
                     </p>
 
@@ -43,7 +46,11 @@ function Logout({backgroundLocation: logoutBackgroundLocation}: LogoutProps) {
                     <div className="flex gap-3">
                         <button
                             type="button"
-                            className="h-[3.125rem] w-[12.4375rem] rounded-[0.625rem] bg-[#2D2D2D] text-[#C8C8C8] hover:bg-[#4c4c4c]"
+                            className={`h-[3.125rem] w-[12.4375rem] rounded-[0.625rem] transition-colors ${
+                                isDarkMode
+                                    ? 'bg-[#2D2D2D] text-[#C8C8C8] hover:bg-[#4c4c4c]'
+                                    : 'bg-[#e1e1e1] text-[#5d5d5d] hover:bg-[#cccccc]'
+                            }`}
                             onClick={() => navigate('/home')}>
                             돌아가기
                         </button>
@@ -57,7 +64,10 @@ function Logout({backgroundLocation: logoutBackgroundLocation}: LogoutProps) {
                 </>
             ) : (
                 <>
-                    <p className="mb-[2rem] mt-[2.2rem] text-[0.98rem] font-medium text-white">
+                    <p
+                        className={`mb-[2rem] mt-[2.2rem] text-[0.98rem] font-medium ${
+                            isDarkMode ? 'text-white' : 'text-black'
+                        }`}>
                         로그인을 진행해 주세요
                     </p>
 
@@ -65,7 +75,11 @@ function Logout({backgroundLocation: logoutBackgroundLocation}: LogoutProps) {
                     <div className="flex gap-3">
                         <button
                             type="button"
-                            className="h-[3.125rem] w-[12.4375rem] rounded-[0.625rem] bg-[#2D2D2D] text-[#C8C8C8] hover:bg-[#4c4c4c]"
+                            className={`h-[3.125rem] w-[12.4375rem] rounded-[0.625rem] transition-colors ${
+                                isDarkMode
+                                    ? 'bg-[#2D2D2D] text-[#C8C8C8] hover:bg-[#4c4c4c]'
+                                    : 'bg-[#e1e1e1] text-[#5d5d5d] hover:bg-[#cccccc]'
+                            }`}
                             onClick={() => navigate('/home')}>
                             돌아가기
                         </button>
